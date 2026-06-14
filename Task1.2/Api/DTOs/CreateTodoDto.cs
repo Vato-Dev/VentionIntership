8e5e0277e1df7bf574612f9f612315cf7ce0333b
VatoDev <vladimer.osmanovidoit@geolab.edu.ge>
2026-06-14 15:36:16 +0400

    Initial empty commit

---
+++ Task1.2/Api/DTOs/CreateTodoDto.cs
@@ -0,0 +1,9 @@
+﻿using Api.Models;
+
+namespace Api.DTOs
+{
+    public sealed record CreateTodoDto(string Description); //it's immutable after creation and their equality is defined by comparing the values not references
+    public sealed record UpdateTodoDto(string Description);
+    public sealed record ChangeTodoStatusDto(Status Status);
+    
+}
