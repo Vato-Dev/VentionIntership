e1fc215d6b33be67a6dbdf33a30103c04ad11869
VatoDev <vladimer.osmanovidoit@geolab.edu.ge>
2026-07-05 16:54:58 +0400

    updating db applying indexes

--- Task3.2/Persistence/Persistence/Migrations/20260705125420_apply index.Designer.cs
+++ Task3.2/Persistence/Persistence/Migrations/20260705125420_apply index.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Persistence;
 
 #nullable disable
 
-namespace Persistence.Migrations
+namespace Persistence.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260705125420_apply index")]
+    partial class applyindex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
