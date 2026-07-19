ab2d1590c0b502546122203fef62c9df9d783040
VatoDev <vladimer.osmanovidoit@geolab.edu.ge>
2026-07-19 20:17:57 +0400

    Implementing simple grpc (using nugets in .net ecosyste, not protobuff compiler directly)

---
+++ SimpleGrpc/GrpcServer/Services/CalculatorService.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Core;
+using SharedContracts;
+
+namespace GrpcServer.Services;
+
+public class CalculatorService : Calculator.CalculatorBase
+{
+    public override Task<AddReply> Add(AddRequest request, ServerCallContext context)
+    {
+        int sum = request.NumberA + request.NumberB;
+
+        return Task.FromResult(new AddReply
+        {
+            Result = sum
+        });
+    }
+}
