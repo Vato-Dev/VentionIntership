0b85194ae1e91f77f24fcf911ad38f389ce004de
VatoDev <vladimer.osmanovidoit@geolab.edu.ge>
2026-07-26 21:36:12 +0400

    init taskproject (plannned to make clients/servers simulation beside Swagger)

---
+++ FileUploadTask/Api/Controllers/FileUploadController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace Api.Controllers
+{
+    [Route("/api/[controller]")]
+    public sealed class FileUploadController : ControllerBase
+    {
+
+        public readonly string UploadPath =  $"{Directory.GetCurrentDirectory()}/uploads";
+     /*   [HttpPost]
+        public IActionResult Upload(List<IFormFile> files) // I could use IFormFile it's easy , but for understanding theme better i won't use it
+        {
+
+            if(files.Count == 0)
+                return  BadRequest("No files selected");
+        }*/
+
+     [HttpPost]
+     public IActionResult Upload()
+     {
+    
+         var boundary = HeaderUtilities.RemoveQuotes(MediaTypeHeaderValue)
+     }
+     
+}
+}
