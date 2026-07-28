cc85aead5cea63cbf52ba1af10e2091a4c898a1f
VatoDev <vladimer.osmanovidoit@geolab.edu.ge>
2026-07-28 20:39:58 +0400

    forgot to upload tasks...

--- FileUploadTask/Api/Program.cs
+++ FileUploadTask/Api/Program.cs
@@ -12,6 +12,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(op=>{ 
+        op.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
