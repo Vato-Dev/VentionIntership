4ef5b67ddfc497ffcf6c6eb4ce2b8fbd0db195fc
VatoDev <vladimer.osmanovidoit@geolab.edu.ge>
2026-08-09 16:37:42 +0400

    correctly working task

---
+++ w9.2/w9.2/Program.cs
@@ -0,0 +1,51 @@
+﻿using MassTransit;
+using MassTransit.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using w9._2.WorkerMock;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing =>
+    {
+        tracing
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("OrderWorker"))
+            .AddSource(DiagnosticHeaders.DefaultListenerName) 
+            .AddOtlpExporter(); 
+    });
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<OrderProcessedConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        cfg.ReceiveEndpoint("order-processing-queue", e =>
+        {
+            e.ConcurrentMessageLimit = 2;  //because of this constraint 
+            e.ConfigureConsumer<OrderProcessedConsumer>(context);
+        });
+    });
+});
+
+var host = builder.Build();
+var runTask = host.RunAsync();
+//to not make things simpler i'll just send this messages at start
+var bus = host.Services.GetRequiredService<IBus>();
+for (int i = 0; i < 10; i++)
+{
+    await bus.Publish(new OrderProcessedEvent(Guid.NewGuid()));
+    Console.WriteLine($"Published message {i + 1}");
+}
+
+await runTask;
---
+++ w9.2/w9.2/WorkerMock/OrderProcessedConsumer.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+
+namespace w9._2.WorkerMock
+{
+    public record OrderProcessedEvent(Guid OrderId);
+
+    public class OrderProcessedConsumer : IConsumer<OrderProcessedEvent>
+    {
+        public async Task Consume(ConsumeContext<OrderProcessedEvent> context)
+        {
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] START  {context.Message.OrderId}");
+            await Task.Delay(1500);
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] FINISH {context.Message.OrderId}");
+        }
+    }
+}
