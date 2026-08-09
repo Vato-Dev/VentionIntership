eaf55b8601f1b7e7cbcfb85379d48732b4d06236
VatoDev <vladimer.osmanovidoit@geolab.edu.ge>
2026-08-09 07:24:15 +0400

    basic rabbit practice

---
+++ Week8Redo/PureRabbitClient/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName =  "localhost" };
+factory.UserName = "guest";
+factory.Password = "guest";
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+
+await channel.ExchangeDeclareAsync("exchange", ExchangeType.Topic);
+
+var routingKey = args.Length > 0 ? args[0] : "nothing.there";
+var message = args.Length > 1 ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync("exchange", routingKey, body);
+
+Console.WriteLine(" [x] Sent message {0}", message);
---
+++ Week8Redo/PureReceiver/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+if (args.Length < 1)
+{
+    Console.WriteLine("Usage: {0} <routingKey>", Environment.GetCommandLineArgs()[0]);
+    Console.ReadKey();
+    Environment.ExitCode = 1;
+}
+
+var factory = new ConnectionFactory { HostName =  "localhost" };
+factory.UserName = "guest";
+factory.Password = "guest";
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync("exchange", ExchangeType.Topic);
+
+var queueDeclareResult = await channel.QueueDeclareAsync();
+var queueName = queueDeclareResult.QueueName;
+
+foreach (string? bindingKey in args)
+{
+    await channel.QueueBindAsync(queue: queueName, exchange: "exchange", routingKey: bindingKey);
+
+}
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var routingKey = ea.RoutingKey;
+    Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(queueName, autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
